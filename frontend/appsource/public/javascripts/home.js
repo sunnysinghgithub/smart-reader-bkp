671484de6d99456eebf0b6fd6f232e907a05c41b
Ubuntu <sunnysingh1985>
2015-05-16 20:44:17 -0700

    added gem css

--- frontend/appsource/public/javascripts/home.js
+++ frontend/appsource/public/javascripts/home.js
@@ -2,12 +2,22 @@ var Home = (function() {
 	return 	{
 		onload : function() {
 			$('#form-main').submit(Home.findgems);
+			$.fn.wrapInTag = function(opts) {
+			  var tag = opts.tag || 'strong'
+			    , words = opts.words || []
+			    , regex = RegExp(words.join('|'), 'gi') // case insensitive
+			    , replacement = '<'+ tag +'>$&</'+ tag +'>';
+
+			  return this.html(function() {
+			    return $(this).text().replace(regex, replacement);
+			  });
+			};
 		},
 
 		findgems : function(e) {
 			e.preventDefault();
 			// Get the usertext
-			var usertext  = {};
+			usertext  = {};
 			usertext.body = $('#usertext').val();
 			$.ajax({
 				method: "POST",
@@ -21,7 +31,17 @@ var Home = (function() {
 
 		rendergems : function(response) {
 			alert(response.gems);
+		 	gems = response.gems.split(', ');
+		 	userText = $('#usertext').val()
+		 	for(i=0;i<gems.length;i++){
+		 		alert(gems[i]);
+		 	}
 			$('#form-main').hide();
+			$('#gems').append(userText);
+			$('#gems').wrapInTag({
+			  tag: 'gem',
+			  words: gems
+			});
 		}
 	};
 })();
